e5bd3c34e32976f7620d8d4f7551e465afab4ab5
Sasha Pachev <alexander.pachev@servicenow.com>
2024-03-21 15:16:39 -0600

    Port to older versions of GDB to work around their bugs

--- gdb_trace_mmap.py
+++ gdb_trace_mmap.py
@@ -10,9 +10,13 @@ def on_exit(ev):
 n_hits = 0
 if 'max_hits' not in globals():
     max_hits = 10 # invoke with --ex "py max_hits=value" to change
-class MmapBreakpoint(gdb.Breakpoint):
+
+if 'funcs' not in globals():
+    funcs = 'mmap,brk'
+
+class MemoryBreakpoint(gdb.Breakpoint):
     def __init__(self, spec):
-        super(MmapBreakpoint, self).__init__(spec, gdb.BP_BREAKPOINT, internal = False)
+        super(MemoryBreakpoint, self).__init__(spec, gdb.BP_BREAKPOINT, internal = False)
 
     def stop(self):
         gdb.execute("bt")
@@ -21,10 +25,25 @@ class MmapBreakpoint(gdb.Breakpoint):
         print("n_hits={}".format(n_hits))
         if n_hits >= max_hits:
             gdb.execute("quit")
+        #safe_cont()
         return False
 
+def safe_cont():
+    gdb.execute("info threads")
+    for i in range(1,100):
+        try:
+            gdb.execute("c")
+            return
+        except:
+            print("Got error: try {}".format(i))
+
 gdb.execute("set pagination 0")
-b = MmapBreakpoint("mmap")
+gdb.execute("set confirm off")
+func_list = funcs.split(",")
+bp_list = []
+
+for f in func_list:
+    bp_list.append(MemoryBreakpoint(f))
 procs = gdb.inferiors()
 if len(procs) < 1:
     die("No process loaded or attached")
@@ -34,3 +53,4 @@ if not inf_proc.was_attached:
     gdb.execute("run")
 else:
     gdb.execute("c")
+
